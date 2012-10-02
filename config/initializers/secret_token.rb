db0d2db6846554095fb76b7637e6aeb37a9aa682
Bruno Pinto <brunoferreirapinto@gmail.com>
2012-10-02 20:53:16 -0300

    Blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lactis::Application.config.secret_token = 'a266e2285ac0e2d555a859e1e53e5feb80fcb9637d4deb716fbdfe4e7e343ccdb169cf53efc4293201b390cdbcaea1e087b33517706ae13b928b507d45d14281'
