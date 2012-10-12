3f5af9fb49c7ea0428108c36270c279a9770d5fb
Bruno Pinto <brunoferreirapinto@gmail.com>
2012-10-12 15:33:53 -0300

    do not output the assets on log files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,6 @@ Lactis::Application.configure do
   # Do not compress assets
   config.assets.compress = false
 
-  # Expands the lines which load the assets
-  config.assets.debug = true
+  # Do not expands the lines which load the assets
+  config.assets.debug = false
 end
